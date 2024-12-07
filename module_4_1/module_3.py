432029f2bdc94cfb18498e6ba9355fd11cbaef68
Катя <katja48tab42794@gmail.com>
2024-12-07 23:26:56 +0300

    07/12

--- module_4_1/module_3.py
+++ module_4_1/module_3.py
@@ -11,6 +11,23 @@
 # <function some_func at 0x00000277CA738CC0>
 
 import modules
+from modules import module_2
 
-print(modules)
-modules.module_1
+print(module_2)
+# Я в ините в пакете модульс
+# Я из второго модуля
+# Я из второго модуля
+#   4           0 RESUME                   0
+#
+#   5           2 LOAD_CONST               1 ('Я из второго модуля')
+#               4 STORE_FAST               0 (a)
+#
+#   6           6 LOAD_GLOBAL              1 (NULL + print)
+#              16 LOAD_CONST               1 ('Я из второго модуля')
+#              18 CALL                     1
+#              26 POP_TOP
+#
+#   7          28 LOAD_FAST                0 (a)
+#              30 RETURN_VALUE
+# <module 'modules.module_2' from 'G:\\pyton\\lesson_md_4_5\\module_4_1\\modules\\module_2.py'>
+
