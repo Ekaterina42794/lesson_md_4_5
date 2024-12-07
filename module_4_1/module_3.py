59e286c08cbf9bea72b12813708779bbd985191a
Катя <katja48tab42794@gmail.com>
2024-12-07 23:16:35 +0300

    07/12

---
+++ module_4_1/module_3.py
@@ -0,0 +1,16 @@
+# from modules import module_2
+# print(module_2)
+
+# <module 'modules.module_2'
+# from 'G:\\pyton\\lesson_md_4_5\\module_4_1\\modules\\module_2.py'>
+# ______________________________________
+
+# from modules.module_2 import  some_func
+# print(some_func)
+
+# <function some_func at 0x00000277CA738CC0>
+
+import modules
+
+print(modules)
+modules.module_2
---
+++ module_4_1/modules/module_1.py
@@ -0,0 +1,7 @@
+from module_2 import some_func as sf
+import sys
+import math
+import random
+import tkinter
+#  import simple_draw
+b= 10
